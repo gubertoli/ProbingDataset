134bec87cb1ff2ed4521290b245ad868d8d863fd
gubertoli <gubertoli@gmail.com>
2019-11-29 18:44:41 -0300

    more data

--- normalTcpStatelessExtract.py
+++ normalTcpStatelessExtract.py
@@ -1,9 +1,8 @@
 import pyshark
 import pandas as pd
 
-
 df = pd.DataFrame(pd.np.empty((0, 23)))     # 23 empty columns (attributes + class)
-targetAttribute = ""
+
 
 def retrieveattribute(packet):
     pkt_to_list = []
@@ -55,56 +54,21 @@ def retrieveattribute(packet):
         except:
             pkt_to_list.append("")
 
-    global targetAttribute
-    pkt_to_list.append(targetAttribute)
+    pkt_to_list.append("normal")
 
     df.loc[len(df)] = pkt_to_list
 
 
 def main():
-    """
-        The tuples are used to define the filter criteria (ip_addr) for extract packets data
-        from pcap files and to define it class
-        ( Source/Attacker IP, Class, Filename )
-    """
 
-    dictpcapfile = (
-        ("192.168.181.140", "nmap_syn", "nmap.pcapng"),
-        ("192.168.181.142", "nmap_connect", "nmap.pcapng"),
-        ("192.168.181.143", "nmap_null", "nmap.pcapng"),
-        ("192.168.181.144", "nmap_xmas", "nmap.pcapng"),
-        ("192.168.181.145", "nmap_fin", "nmap.pcapng"),
-        ("192.168.181.146", "nmap_ack", "nmap.pcapng"),
-        ("192.168.181.147", "nmap_window", "nmap.pcapng"),
-        ("192.168.181.148", "nmap_maimon", "nmap.pcapng"),
-        ("192.168.181.150", "unicorn_syn", "unicornscan.pcapng"),
-        ("192.168.181.152", "unicorn_connect", "unicornscan.pcapng"),
-        ("192.168.181.153", "unicorn_null", "unicornscan.pcapng"),
-        ("192.168.181.154", "unicorn_xmas", "unicornscan.pcapng"),
-        ("192.168.181.155", "unicorn_fxmas", "unicornscan.pcapng"),
-        ("192.168.181.156", "unicorn_fin", "unicornscan.pcapng"),
-        ("192.168.181.157", "unicorn_ack", "unicornscan.pcapng"),
-        ("192.168.181.160", "hping_syn", "hping3.pcapng"),
-        ("192.168.181.162", "hping_null", "hping3.pcapng"),
-        ("192.168.181.163", "hping_xmas", "hping3.pcapng"),
-        ("192.168.181.164", "hping_fin", "hping3.pcapng"),
-        ("192.168.181.165", "hping_ack", "hping3.pcapng"),
-        ("192.168.181.131", "zmap_syn", "zmap_tcp_5k.pcapng"),
-        ("192.168.181.131", "mass_syn", "masscan_5k.pcapng")
-    )
-
-    folder = "pcap"
-
-    for i in dictpcapfile:
-        global targetAttribute
-        targetAttribute = i[1]
-        cap = pyshark.FileCapture(folder + "/" + i[2], display_filter="tcp && ip.src == "+i[0])
-        cap.apply_on_packets(retrieveattribute)
-        cap.close()
+    pcap_normal_traffic_file = "mawilab/normal_without_ipv6.pcapng"
+    cap = pyshark.FileCapture(pcap_normal_traffic_file)
+    cap.apply_on_packets(retrieveattribute)
+    cap.close()
 
     print(df.head())
     print(df['Class'].value_counts())
-    df.to_csv(r'probing.csv', index=None, header=True)
+    df.to_csv(r'normal.csv', index=None, header=True)
 
 if __name__ == "__main__":
     main()
