43867efd06d43a5752d8242849cea699f73757d5
Gustavo Bertoli <gubertoli@gmail.com>
2020-08-31 18:29:15 -0300

    timestamp

--- internet/server/udp_server.py
+++ internet/server/udp_server.py
@@ -1,6 +1,7 @@
 from socket import *
 import subprocess
 import os
+from datetime import datetime
 
 server_port = 12000
 server_socket = socket(AF_INET, SOCK_DGRAM)
@@ -13,7 +14,8 @@ while True:
     print(">> Received message from: ", client_address[0])
 
     stop = ""
-    filename = message.decode('utf-8')[:2]+'.pcap'  # message parsing
+    datehour = str(datetime.today().strftime('%Y%m%d%H%M%S'))
+    filename = message.decode('utf-8')[:2]+'_'+datehour+'.pcap'  # message parsing
 
     # tcpdump -i eth0 tcp -s0 -nn -w captura.pcap
     # -i    | select interface (eth0)
